9841db82bf1921f25307fe1e38d868de5848edea
BrunoVCode <brunonewdev@gmail.com>
2023-03-01 15:03:40 -0300

    Ativ

---
+++ Atividades/01-03/somarDic.py
@@ -0,0 +1,9 @@
+turma01 = {'Alice':8, 'Pedro':7}
+turma02 = {'Leticia':9, 'Isaac':7}
+turma03 = {'Clecia':8, 'Icaro':7}
+turmaTodas = {}
+
+turmaTodas.update(turma01) 
+turmaTodas.update(turma02) 
+turmaTodas.update(turma03) 
+print(turmaTodas)
--- Atividades/27-02/nome.py
+++ Atividades/27-02/nome.py
@@ -2,15 +2,21 @@ from rich.console import Console
 console = Console()
 print('Escreva um nome')
 nome=str(input('> '))
-print(f'A quantidade de letras do nome {nome} é igual a {len(nome)}')
+print(f'A quantidade de letras do nome {nome} é igual a {len(nome)-nome.count(" ")}')
 print(f'A primeira letra do nome de {nome} é {nome[0]}')
-nomeInvertido=(nome[::-1])
+# nomeInvertido=(nome[::-1])
+# print(f'O nome {nome} ao contrario é ', end='')
+# nomeInvertidoEditado = nomeInvertido.capitalize()
+# console.print(nomeInvertidoEditado, style='red b ')
+nomeInvertido = ''
+for x in range(len(nome)):
+    nomeInvertido = nomeInvertido + nome[len(nome)-1-x]
+nomeInvertidoEditado = nomeInvertido.capitalize()
 print(f'O nome {nome} ao contrario é ', end='')
-print((nomeInvertido.capitalize()))
+console.print (nomeInvertidoEditado, style='red b ')
 nomeLetras=[]
 for x in range(len(nome)):
-    if x % 2 == 0:
+    if (x+1) % 2 != 0:
         nomeLetras.append(nome[x])
 print(f'As letras ímpares de {nome}, são ', end='')
 print (*nomeLetras, sep=', ')
-
